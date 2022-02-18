b3cbebfb0d0ca6e951cc640b891d342ba2d8fffe
dishapatel010 <71930916+dishapatel010@users.noreply.github.com>
2022-02-18 18:19:14 +0530

    Update stream_routes.py

--- WebStreamer/server/stream_routes.py
+++ WebStreamer/server/stream_routes.py
@@ -147,7 +147,7 @@ async def media_streamer(request: web.Request, message_id: int, secure_hash: str
 
     return return_resp
 
-@routes.get("/download")
+@routes.get("/download, allow_head=True)
 def download_page():
     try:
         dl_url = request.args['id']
