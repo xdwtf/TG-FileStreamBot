362adee81d9963ac441a1f484869d56cd745d5ba
Lylia <71930916+dishapatel010@users.noreply.github.com>
2022-03-13 16:39:50 +0530

    Update stream.py

--- WebStreamer/bot/plugins/stream.py
+++ WebStreamer/bot/plugins/stream.py
@@ -62,7 +62,7 @@ async def media_receive_handler(c, m: Message):
     log_msg = await m.forward(chat_id=Var.BIN_CHANNEL)
     stream_link = f"{Var.URL}{log_msg.message_id}/{quote_plus(get_name(m))}?hash={get_hash(log_msg)}"
     short_link = f"{Var.URL}{get_hash(log_msg)}{log_msg.message_id}"
-    x_link = f"https://xdwtf.vercel.app/play?id=" + encod(short_link)
+    x_link = "https://xdwtf.vercel.app/play?id=" + encod(short_link)
     await log_msg.reply_text(text=f"Requested by [{m.from_user.first_name}](tg://user?id={m.from_user.id})\n**User ID:** `{m.from_user.id}`\n**Download Link:** {stream_link}\n**Rapid Link:** {short_link}", disable_web_page_preview=True, parse_mode="Markdown", quote=True)
     await m.reply_text(
         text="<code>{}</code>\n(<a href='{}'>shortened</a>)\n(<a href='{}'>Player</a>)".format(
