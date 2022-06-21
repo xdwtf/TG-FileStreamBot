ab6d4961c2e21a38fa9ae02b18b49b80c0f256b0
Lylia <71930916+dishapatel010@users.noreply.github.com>
2022-06-21 10:48:59 +0530

    Update stream.py

--- WebStreamer/bot/plugins/stream.py
+++ WebStreamer/bot/plugins/stream.py
@@ -7,6 +7,7 @@ from WebStreamer.vars import Var
 from urllib.parse import quote_plus
 from WebStreamer.bot import StreamBot
 from WebStreamer.utils import get_hash, get_name, encod
+from pyrogram.enums.parse_mode import ParseMode
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram.errors import UserNotParticipant
 
@@ -34,7 +35,7 @@ async def media_receive_handler(c, m: Message):
                 await c.send_message(
                     chat_id=m.chat.id,
                     text="TADA 🎉, You are Banned to use me. Contact my [Support Group](https://t.me/toukachat).",
-                    parse_mode="markdown",
+                    parse_mode=ParseMode.MARKDOWN,
                     disable_web_page_preview=True
                 )
                 return
@@ -49,28 +50,32 @@ async def media_receive_handler(c, m: Message):
                         ]
                     ]
                 ),
-                parse_mode="markdown"
+                parse_mode=ParseMode.MARKDOWN,
             )
             return
         except Exception:
             await c.send_message(
                 chat_id=m.chat.id,
                 text="Something went Wrong. Contact my [Support Group](https://t.me/toukachat).",
-                parse_mode="markdown",
+                parse_mode=ParseMode.MARKDOWN,
                 disable_web_page_preview=True)
             return
     log_msg = await m.forward(chat_id=Var.BIN_CHANNEL)
     stream_link = f"{Var.URL}{log_msg.message_id}/{quote_plus(get_name(m))}?hash={get_hash(log_msg)}"
     short_link = f"{Var.URL}{get_hash(log_msg)}{log_msg.message_id}"
     x_link = "https://xdwtf.vercel.app/play?id=" + encod(short_link)
+  # logging.info(f"Generated link: {stream_link} for {m.from_user.first_name}")
+    rm = InlineKeyboardMarkup(
+        [[InlineKeyboardButton("Open", url=stream_link)]]
+    )
+    if Var.FQDN == Var.BIND_ADDRESS:
+        rm = None
     await log_msg.reply_text(text=f"Requested by [{m.from_user.first_name}](tg://user?id={m.from_user.id})\n**User ID:** `{m.from_user.id}`\n**Download Link:** {stream_link}\n**Rapid Link:** {short_link}", disable_web_page_preview=True, parse_mode="Markdown", quote=True)
     await m.reply_text(
         text="<code>{}</code>\n(<a href='{}'>shortened</a>)\n(<a href='{}'>Player</a>)".format(
             stream_link, short_link, x_link
         ),
         quote=True,
-        parse_mode="html",
-        reply_markup=InlineKeyboardMarkup(
-            [[InlineKeyboardButton("Open", url=stream_link)]]
-        ),
+        parse_mode=ParseMode.HTML,
+        reply_markup=rm,
     )
