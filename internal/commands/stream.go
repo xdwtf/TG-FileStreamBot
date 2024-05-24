818053d09dc5585b35f75ede7405db5024bd09ef
Lylia <71930916+dishapatel010@users.noreply.github.com>
2024-05-24 13:24:40 +0530

    Update stream.go

--- internal/commands/stream.go
+++ internal/commands/stream.go
@@ -6,129 +6,145 @@ import (
 
 	"EverythingSuckz/fsb/config"
 	"EverythingSuckz/fsb/internal/utils"
-
+	
 	"github.com/celestix/gotgproto"
 	"github.com/celestix/gotgproto/dispatcher"
-	"github.com/celestix/gotgproto/dispatcher/handlers"
-	"github.com/celestix/gotgproto/ext"
-	"github.com/celestix/gotgproto/storage"
-	"github.com/celestix/gotgproto/types"
-	"github.com/gotd/td/telegram/message/styling"
-	"github.com/gotd/td/tg"
+        "github.com/celestix/gotgproto/dispatcher/handlers"
+        "github.com/celestix/gotgproto/ext"
+        "github.com/celestix/gotgproto/storage"
+        "github.com/celestix/gotgproto/types"
+        "github.com/gotd/td/telegram/message/styling"
+        "github.com/gotd/td/tg"
 )
 
-func (m *command) LoadStream(dispatcher dispatcher.Dispatcher) {
-	log := m.log.Named("start")
-	defer log.Sugar().Info("Loaded")
-	dispatcher.AddHandler(
-		handlers.NewMessage(nil, sendLink),
-	)
+func(m * command) LoadStream(dispatcher dispatcher.Dispatcher) {
+        log: = m.log.Named("start")
+        defer log.Sugar().Info("Loaded")
+        dispatcher.AddHandler(
+                handlers.NewMessage(nil, sendLink),
+        )
 }
 
-func supportedMediaFilter(m *types.Message) (bool, error) {
-	if not := m.Media == nil; not {
-		return false, dispatcher.EndGroups
-	}
-	switch m.Media.(type) {
-	case *tg.MessageMediaDocument:
-		return true, nil
-	case *tg.MessageMediaPhoto:
-		return false, nil
-	case tg.MessageMediaClass:
-		return false, dispatcher.EndGroups
-	default:
-		return false, nil
-	}
+func supportedMediaFilter(m * types.Message)(bool, error) {
+        if not: = m.Media == nil;
+        not {
+                return false, dispatcher.EndGroups
+        }
+        switch m.Media.(type) {
+                case *tg.MessageMediaDocument:
+                        return true, nil
+                case *tg.MessageMediaPhoto:
+                        return false, nil
+                case tg.MessageMediaClass:
+                        return false, dispatcher.EndGroups
+                default:
+                        return false, nil
+        }
 }
 
-func sendLink(ctx *ext.Context, client *gotgproto.Client, u *ext.Update) error {
-	chatId := u.EffectiveChat().GetID()
-	peerChatId := ctx.PeerStorage.GetPeerById(chatId)
-	if peerChatId.Type != int(storage.TypeUser) {
-		return dispatcher.EndGroups
-	}
-	if len(config.ValueOf.AllowedUsers) != 0 && !utils.Contains(config.ValueOf.AllowedUsers, chatId) {
-		ctx.Reply(u, "You are not allowed to use this bot.", nil)
-		return dispatcher.EndGroups
-	}
-	channelId := config.ValueOf.FChannelID
-        member, err := client.API().GetChatMember(channelId, chatId)
+func sendLink(ctx * ext.Context, u * ext.Update) error {
+        chatId: = u.EffectiveChat().GetID()
+        userId: = u.EffectiveUser().GetID()
+        channelId: = int64(-1001321487892) // Replace with the actual ID of @nexiuo
+        channelAccessHash: = int64(0) // Replace with the actual access hash of @nexiuo if needed
+
+        // Check if the user has joined the @nexiuo channel
+                member,
+        err: = ctx.API().ChannelsGetParticipant(context.TODO(), & tg.ChannelsGetParticipantRequest {
+                Channel: & tg.InputChannel {
+                        ChannelID: channelId,
+                        //AccessHash: channelAccessHash,
+                },
+                Participant: & tg.InputPeerUser {
+                        UserID: userId,
+                },
+        })
+        if err != nil || member == nil {
+                ctx.Reply(u, "You need to join the @nexiuo channel to use this bot.", nil)
+                return dispatcher.EndGroups
+        }
+
+        peerChatId: = ctx.PeerStorage.GetPeerById(chatId)
+        if peerChatId.Type != int(storage.TypeUser) {
+                return dispatcher.EndGroups
+        }
+        if len(config.ValueOf.AllowedUsers) != 0 && !utils.Contains(config.ValueOf.AllowedUsers, chatId) {
+                ctx.Reply(u, "You are not allowed to use this bot.", nil)
+                return dispatcher.EndGroups
+        }
+        supported,
+        err: = supportedMediaFilter(u.EffectiveMessage)
         if err != nil {
-	    utils.Logger.Sugar().Error(err)
-            ctx.Reply(u, fmt.Sprintf("Error - %s", err.Error()), nil)
-            return dispatcher.EndGroups
+                return err
         }
-        if member.Status == "left" || member.Status == "kicked" {
-            ctx.Reply(u, fmt.Sprintf("Please join our channel to use this bot: @nexiuo"), nil)
-            return dispatcher.EndGroups
+        if !supported {
+                ctx.Reply(u, "Sorry, this message type is unsupported.", nil)
+                return dispatcher.EndGroups
         }
-	supported, err := supportedMediaFilter(u.EffectiveMessage)
-	if err != nil {
-		return err
-	}
-	if !supported {
-		ctx.Reply(u, "Sorry, this message type is unsupported.", nil)
-		return dispatcher.EndGroups
-	}
-	update, err := utils.ForwardMessages(ctx, chatId, config.ValueOf.LogChannelID, u.EffectiveMessage.ID)
-	if err != nil {
-		utils.Logger.Sugar().Error(err)
-		ctx.Reply(u, fmt.Sprintf("Error - %s", err.Error()), nil)
-		return dispatcher.EndGroups
-	}
-	messageID := update.Updates[0].(*tg.UpdateMessageID).ID
-	if err != nil {
-		utils.Logger.Sugar().Error(err)
-		ctx.Reply(u, fmt.Sprintf("Error - %s", err.Error()), nil)
-		return dispatcher.EndGroups
-	}
-	doc := update.Updates[1].(*tg.UpdateNewChannelMessage).Message.(*tg.Message).Media
-	file, err := utils.FileFromMedia(doc)
-	if err != nil {
-		ctx.Reply(u, fmt.Sprintf("Error - %s", err.Error()), nil)
-		return dispatcher.EndGroups
-	}
-	fullHash := utils.PackFile(
-		file.FileName,
-		file.FileSize,
-		file.MimeType,
-		file.ID,
-	)
-	hash := utils.GetShortHash(fullHash)
-	link := fmt.Sprintf("%s/stream/%d?hash=%s", config.ValueOf.Host, messageID, hash)
-	text := []styling.StyledTextOption{styling.Code(link)}
-	row := tg.KeyboardButtonRow{
-		Buttons: []tg.KeyboardButtonClass{
-			&tg.KeyboardButtonURL{
-				Text: "Download",
-				URL:  link + "&d=true",
-			},
-		},
-	}
-	if strings.Contains(file.MimeType, "video") || strings.Contains(file.MimeType, "audio") || strings.Contains(file.MimeType, "pdf") {
-		row.Buttons = append(row.Buttons, &tg.KeyboardButtonURL{
-			Text: "Stream",
-			URL:  link,
-		})
-	}
-	markup := &tg.ReplyInlineMarkup{
-		Rows: []tg.KeyboardButtonRow{row},
-	}
-	if strings.Contains(link, "http://localhost") {
-		_, err = ctx.Reply(u, text, &ext.ReplyOpts{
-			NoWebpage:        false,
-			ReplyToMessageId: u.EffectiveMessage.ID,
-		})
-	} else {
-		_, err = ctx.Reply(u, text, &ext.ReplyOpts{
-			Markup:           markup,
-			NoWebpage:        false,
-			ReplyToMessageId: u.EffectiveMessage.ID,
-		})
-	}
-	if err != nil {
-		utils.Logger.Sugar().Error(err)
-		ctx.Reply(u, fmt.Sprintf("Error - %s", err.Error()), nil)
-	}
-	return dispatcher.EndGroups
+        update,
+        err: = utils.ForwardMessages(ctx, chatId, config.ValueOf.LogChannelID, u.EffectiveMessage.ID)
+        if err != nil {
+                utils.Logger.Sugar().Error(err)
+                ctx.Reply(u, fmt.Sprintf("Error - %s", err.Error()), nil)
+                return dispatcher.EndGroups
+        }
+        messageID: = update.Updates[0].( * tg.UpdateMessageID).ID
+        if err != nil {
+                utils.Logger.Sugar().Error(err)
+                ctx.Reply(u, fmt.Sprintf("Error - %s", err.Error()), nil)
+                return dispatcher.EndGroups
+        }
+        doc: = update.Updates[1].( * tg.UpdateNewChannelMessage).Message.( * tg.Message).Media
+        file,
+        err: = utils.FileFromMedia(doc)
+        if err != nil {
+                ctx.Reply(u, fmt.Sprintf("Error - %s", err.Error()), nil)
+                return dispatcher.EndGroups
+        }
+        fullHash: = utils.PackFile(
+                file.FileName,
+                file.FileSize,
+                file.MimeType,
+                file.ID,
+        )
+        hash: = utils.GetShortHash(fullHash)
+        link: = fmt.Sprintf("%s/stream/%d?hash=%s", config.ValueOf.Host, messageID, hash)
+        text: = [] styling.StyledTextOption {
+                styling.Code(link)
+        }
+        row: = tg.KeyboardButtonRow {
+                Buttons: [] tg.KeyboardButtonClass { & tg.KeyboardButtonURL {
+                                Text: "Download",
+                                URL: link + "&d=true",
+                        },
+                },
+        }
+        if strings.Contains(file.MimeType, "video") || strings.Contains(file.MimeType, "audio") || strings.Contains(file.MimeType, "pdf") {
+                row.Buttons = append(row.Buttons, & tg.KeyboardButtonURL {
+                        Text: "Stream",
+                        URL: link,
+                })
+        }
+        markup: = & tg.ReplyInlineMarkup {
+                Rows: [] tg.KeyboardButtonRow {
+                        row
+                },
+        }
+        if strings.Contains(link, "http://localhost") {
+                _, err = ctx.Reply(u, text, & ext.ReplyOpts {
+                        NoWebpage: false,
+                        ReplyToMessageId: u.EffectiveMessage.ID,
+                })
+        } else {
+                _, err = ctx.Reply(u, text, & ext.ReplyOpts {
+                        Markup: markup,
+                        NoWebpage: false,
+                        ReplyToMessageId: u.EffectiveMessage.ID,
+                })
+        }
+        if err != nil {
+                utils.Logger.Sugar().Error(err)
+                ctx.Reply(u, fmt.Sprintf("Error - %s", err.Error()), nil)
+        }
+        return dispatcher.EndGroups
 }
